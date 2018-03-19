c1bda15b18a45beffab689f3c3ad87134c2703fc
Jack <jackdeng@gmail.com>
2018-03-18 18:18:52 -0700

    as library

--- amr2wav.go
+++ amr2wav.go
@@ -1,4 +1,4 @@
-package main
+package amr2wav
 
 import (
 	"bytes"
---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	amr2wav ".."
+	"io/ioutil"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s inputfile outputfile\n", os.Args[0])
+		return
+	}
+
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	out, err := amr2wav.Convert(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	ioutil.WriteFile(os.Args[2], out, 0755)
+	log.Printf("Converted %s to %s\n", os.Args[1], os.Args[2])
+}
